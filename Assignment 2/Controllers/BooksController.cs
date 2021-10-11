a5ac92a8d5e5dd367e1b3b9e364b6c47484f3247
Ziaullah Hassan <Ziaullah Hassan@DESKTOP-2U19JOA>
2021-10-11 12:44:32 -0700

    implementing the  simple CRUD operations for the model class Book (Assignment 1).

---
+++ Assignment 2/Controllers/BooksController.cs
@@ -0,0 +1,61 @@
+﻿using Assignment_2.Managers;
+using Assignment1;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Assignment_2.Controllers
+{
+    //api/Books
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private BookManager _manager = new BookManager();
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{ISBN13}")]
+        public Book Get(String ISBN13)
+        {
+            return _manager.Get(ISBN13);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public Book Post([FromBody] Book book )
+        {
+           // var book = JsonSerializer.Deserialize<Book>(value);
+           
+            return _manager.create(book);
+            
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{ISBN13}")]
+        public Book Put(String ISBN13, [FromBody] Book book)
+        {
+           // var book = JsonSerializer.Deserialize<Book>(value);
+            return _manager.Update(ISBN13, book);
+
+            
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{ISBN13}")]
+        public Book Delete(String ISBN13)
+        {
+            return _manager.Delete(ISBN13);
+        }
+    }
+}
---
+++ Assignment 2/Managers/BookManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment1;
+
+namespace Assignment_2.Managers
+{
+    public class BookManager
+    {
+        private static List<Book> _Books = new List<Book>() {
+            new Book ("c# programming","Tom",200,"978-3-16-148410-0"),
+            new Book ("Java","Zia",300,"977-3-16-158410-0"),
+            new Book ("c++","khan",400,"976-3-16-168410-0")
+            };
+            
+        public List<Book> GetAll()
+        {
+            return new List<Book>(_Books);
+
+
+        }
+
+        public Book Get(String ISBN13)
+        {
+            return _Books.Find(book => book.ISBN13 == ISBN13);
+        }
+
+        public Book create(Book book)
+        {
+            var _book = _Books.Find(boook => boook.ISBN13 == book.ISBN13);
+            if (_book == null)
+            {
+
+                _Books.Add(book);
+
+                return book;
+            }
+            return null;
+        }
+        public Book Update(String ISBN13,Book book)
+        {
+          var _book=  _Books.Find(book => book.ISBN13 == ISBN13);
+            _book.Author = book.Author;
+            _book.Title = book.Title;
+            _book.PageNumber = book.PageNumber;
+            return _book;
+
+
+
+        }
+        public Book Delete(String ISBN13)
+        {
+            var _book = _Books.Find(book => book.ISBN13 == ISBN13);
+            _Books.Remove(_book);
+            return _book;
+        }
+        
+
+        }
+    }
+
